7c3225efa155a7becd2d81a000a503503b809b93
Sourangsu Mukherjee <sourangsumukherjee@Sourangsus-MacBook-Air.local>
2018-10-28 21:08:28 +0530

    Update method in mongo Db
    Tracking-Id: Sournagsu

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -10,15 +10,15 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp',(err, db) =>{
   }
   console.log('Connected to MongoDB server');
 
-//   db.collection('Todos').findOneAndUpdate({
-//     _id : new ObjectID("5bd1f2123288179751e3e06a")},
-//   { $set : {
-//     completed : true
-//   }},
-// { returnOriginal : false})
-// .then((res) => {
-//   console.log(res);
-// })
+  db.collection('Todos').findOneAndUpdate({
+    _id : new ObjectID("5bd1f2123288179751e3e06a")},
+  { $set : {
+    completed : true
+  }},
+{ returnOriginal : false})
+.then((res) => {
+  console.log(res);
+})
 
 db.collection('Users').findOneAndUpdate({
   _id : new ObjectID("5bd13799fb133e167721dd13")},
