ad431358204bdd312b2652dd2d7f5cddbde87f18
Sourangsu Mukherjee <sourangsumukherjee@Sourangsus-MacBook-Air.local>
2018-10-31 08:14:15 +0530

    Getting a particular resource
    Tracking-Id: Sourangsu

--- playground/mongodb-queries.js
+++ playground/mongodb-queries.js
@@ -5,25 +5,25 @@ const {User} =require('./../server/models/user')
 
 var id = "5bd20c03116b01197b13d605";
 
-// if(!ObjectID.isValid(id)){
-//   console.log('ID not valid');
-// }
+if(!ObjectID.isValid(id)){
+  console.log('ID not valid');
+}
 
-// Todo.find({ _id : id}).then((todos) => {
-//   console.log('Todos',todos);
-// }).catch((err) => {
-//   console.log(err);
-// });
-//
-// Todo.findOne({ _id : id}).then((todos) => {
-//   console.log('Todo',todos);
-// }).catch((err) => {
-//   console.log(err);
-// });
+Todo.find({ _id : id}).then((todos) => {
+  console.log('Todos',todos);
+}).catch((err) => {
+  console.log(err);
+});
 
-// Todo.findById(id).then((todo) => {
-//   console.log('Todo by ID',todo);
-// }).catch((e)=>console.log(e))
+Todo.findOne({ _id : id}).then((todos) => {
+  console.log('Todo',todos);
+}).catch((err) => {
+  console.log(err);
+});
+
+Todo.findById(id).then((todo) => {
+  console.log('Todo by ID',todo);
+}).catch((e)=>console.log(e))
 
 User.findById(id).then((user)=>{
   if(!user){
