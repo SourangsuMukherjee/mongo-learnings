3485470cd2e1075a680500cc6216c33e6cf12c7b
Sourangsu Mukherjee <sourangsumukherjee@Sourangsus-MacBook-Air.local>
2018-11-06 17:15:54 +0530

    Todos DELETE route
    Tracking-Id: Sourangsu

--- server/server.js
+++ server/server.js
@@ -49,6 +49,19 @@ app.get('/todos/:id',(req,res) =>{
 
 });
 
+app.delete('/todos/:id', (req,res) => {
+  var hexId = req.params.id;
+  if(!ObjectID.isValid(hexId)){
+    return res.status(404).send();
+  }
+  Todo.findByIdAndRemove(hexId).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo})
+  }).catch((err) => res.status(400).send());
+});
+
 app.listen(port, () => {
   console.log(`Started server on port ${port}`);
 })
